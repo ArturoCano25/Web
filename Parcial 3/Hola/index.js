e8ef95db519878fa5ee352bf2527b92817e9ec08
Cano <canoarturo25@hotmail.com>
2024-11-08 07:47:28 -0600

    11-08-2024

--- Parcial 3/Hola/index.js
+++ Parcial 3/Hola/index.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.text());
 
-// app.use((req,res,next) => {
-//     res.send();
-//     next();
-// })
-
-app.get('/empleado', (req, res) => {
+app.get('/administrativos', (req, res) => {
     console.log(req.query);
     res.send('Servidor contestado a peticion get');
 })
 
-app.post('/empleado', (req, res) => {
-    res.send('Servidor contestado a peticion post');
+app.get('/maestros', (req, res) => {
+    console.log(req.body);
+    res.send('Servidor contestado a peticion get');
 })
 
-app.all((req, res) => {
-    res.send('Ruta no encontrada')
+app.get('/estudiantes/:carrera', (req, res) => {
+    console.log(req.params.carrera);
+    console.log(req.query.control);
+    res.send('Servidor contestado a peticion get');
 })
 
-app.listen(8080, () => {
-    console.log('Servidor express escuchado en puerto 8080');
+// app.post('/empleado', (req, res) => {
+//     res.send('Servidor contestado a peticion post');
+// })
+
+// app.all((req, res) => {
+//     res.send('Ruta no encontrada')
+// })
+
+app.listen(8000, () => {
+    console.log('Servidor express escuchado en puerto 8000');
 })
