b9ce6d5ec2fa8e2cab7ffe2a058da1a52dd8d6c7
Cano <canoarturo25@hotmail.com>
2024-11-13 07:27:31 -0600

    11-13-2024

---
+++ Parcial 3/Form_data/formulario.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.text());
+
+const folder = path.join(__dirname + '/archivos/');
+const upload = multer({dest:folder});
+
+app.use(upload.single('archivo'));
+
+app.post('/formulario',(req,res)=>{
+    console.log(req.body)
+    res.render(`Hola ${req.body.nombre}`);
+});
+
+app.listen(8100,()=>{
+    console.log(`Escuchando en el puerto : ${8100}`)
+});
