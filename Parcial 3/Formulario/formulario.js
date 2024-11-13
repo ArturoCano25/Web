9407039432b86d32f149ea5371af26385bbca821
Cano <canoarturo25@hotmail.com>
2024-11-13 07:18:49 -0600

    11-13-2024

---
+++ Parcial 3/Formulario/formulario.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended:true}));
+
+app.post('/formulario',(req,res)=>{
+    console.log(req.body)
+    res.render(`Hola ${req.body.nombre}`);
+});
+
+app.listen(8100,()=>{
+    console.log(`Escuchando en el puerto : ${8100}`)
+});
