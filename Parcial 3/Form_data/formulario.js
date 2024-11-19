82ad72436f920718e6f30105d59ec253225d322f
Cano <canoarturo25@hotmail.com>
2024-11-19 08:01:23 -0600

    11-19-2024

--- Parcial 3/Form_data/formulario.js
+++ Parcial 3/Form_data/formulario.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
+const cors = require('cors');
+const {jsPDF} = require('jspdf');
+const fs = require('fs');
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const dir = path.join(__dirname, '/archivosrec/');
+        cb(null, dir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}-${file.originalname}`);
+    },
+});
+
+const upload = multer({storage: storage})
 
 app.use(express.json());
 app.use(express.text());
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors());
 
-const folder = path.join(__dirname + '/archivos/');
-const upload = multer({dest:folder});
+// const folder = path.join(__dirname + '/archivos/');
+// const upload = multer({dest:folder});
 
-app.use(upload.single('archivo'));
+app.post('/formulario', upload.single('archivos'), (req, res) => {
+    const { nombre, apellido } = req.body;
+    const archivo = req.file;
 
-app.post('/formulario',(req,res)=>{
-    console.log(req.body)
-    res.send(`Hola ${req.body.nombre}`);
+    if (!nombre || !apellido || !archivo) {
+        return res.status(400).send('Todos los campos son obligatorios y se necesita un archivo.');
+    }
+
+    const imagePath = archivo.path;
+    const imageData = fs.readFileSync(imagePath);
+    const imageBase64 = imageData.toString('base64');
+    const imageExtension = path.extname(imagePath).slice(1);
+
+    const doc = new jsPDF();
+    doc.text(`Hola ${nombre} ${apellido}`, 10, 10);
+
+    const imageFormat = imageExtension === 'jpg' ? 'JPEG' : 'PNG';
+    doc.addImage(`data:image/${imageFormat};base64,${imageBase64}`, imageFormat, 10, 20, 100, 100);
+
+    const pdfBuffer = doc.output('arraybuffer');
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="documento.pdf"');
+
+    res.send(Buffer.from(pdfBuffer));
 });
 
 app.listen(8100,()=>{
